27e75b68ace4c50f44cbbc255deac84e53562c8b
boby-front <delanglebob38@gmail.com>
2023-11-28 22:52:53 +0100

    correct swager

--- argentbank/src/components/Logo.js
+++ argentbank/src/components/Logo.js
@@ -6,7 +6,6 @@ const Logo = () => {
   return (
     <NavLink to="/Home" className="main-nav-logo">
       <img className="main-nav-logo-image" src={logo} alt="Argent Bank Logo" />
-      <h1 className="sr-only">Argent Bank</h1>
     </NavLink>
   );
 };
